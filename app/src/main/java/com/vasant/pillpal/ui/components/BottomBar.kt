1d9a26c8ce80346a424aeacb4eedfd58b5b5a15e
Your Name <you@example.com>
2025-06-11 18:18:37 +0530

    feat: implement basic bottom navigation bar
    Adds a basic bottom navigation bar with three icons: home, add, and guides.
    The home and guides icons are vector drawables, replacing the previous PNG for the home icon.
    The BottomNavigationBar composable is created and integrated into the HomeScreen.

---
+++ app/src/main/java/com/vasant/pillpal/ui/components/BottomBar.kt
@@ -0,0 +1,78 @@
+package com.vasant.pillpal.ui.components
+
+import android.widget.Space
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vasant.pillpal.R
+import com.vasant.pillpal.ui.theme.BackgroundColor
+
+@Preview
+@Composable
+fun BottomNavigationBar() {
+    val isSelected by remember {
+        mutableStateOf(false)
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                BackgroundColor
+            ), verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = {}) {
+            Box() {
+                Image(
+                    painter = painterResource(R.drawable.home),
+                    contentDescription = "Home Icon",
+                    modifier = Modifier.size(30.dp), contentScale = ContentScale.Fit,
+                    colorFilter = ColorFilter.tint(Color.Gray)
+                )
+
+            }
+        }
+        Spacer(Modifier.weight(1f))
+        IconButton(onClick = {}) {
+            Box() {
+
+                Icon(
+                    painter = painterResource(R.drawable.add),
+                    tint = Color.Unspecified,
+                    contentDescription = "Home Icon",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+        }
+        Spacer(Modifier.weight(1f))
+        IconButton(onClick = {}) {
+            Box {
+                Image(
+                    painter = painterResource(R.drawable.guides),
+                    contentDescription = "Home Icon",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/vasant/pillpal/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/vasant/pillpal/ui/screens/HomeScreen.kt
@@ -5,15 +5,15 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vasant.pillpal.ui.components.BottomNavigationBar
 import com.vasant.pillpal.ui.components.HomeContent
 import com.vasant.pillpal.ui.components.TopBarHomeScreen
 
 @Preview
 @Composable
-fun HomeScreen(){
-    Scaffold (topBar = { TopBarHomeScreen() }
-        , bottomBar = {}
-    ){
+fun HomeScreen() {
+    Scaffold(topBar = { TopBarHomeScreen() }, bottomBar = { BottomNavigationBar() }
+    ) {
         HomeContent(it)
     }
 }
